2194b856fd6601995fb44379af55c3f9968e353e
Drewster0128 <acook21@hawk.iit.edu>
2025-12-04 12:02:45 -0600

    import poster

--- src/Views/PresentationsView.vue
+++ src/Views/PresentationsView.vue
@@ -1,6 +1,7 @@
 <script setup lang="js">
   import { PresentationObject } from '../types';
   import PresentationBlock from '../components/PresentationBlock.vue';
+  import Poster from '../assets/LFA POSTER FHAA_FH-v10.jpg'
 
   const presentationList = [
     new PresentationObject(
@@ -12,7 +13,7 @@
       "Anti-FH Immune Complexes in MGRS-C3G",
       "International Complement Society Conference",
       "https://iowa-my.sharepoint.com/:p:/g/personal/scook8_uiowa_edu/EfFcv6evfFFNqUsa9SraAg0BsUZfd71-dYL_Q0tYXGF6Kw?e=qKBuDF&xsdata=MDV8MDJ8c3RlcGhhbmllLWNvb2tAdWlvd2EuZWR1fGNiMjkwZmZmZThmZDRhNzk5ZTc4MDhkZTE0ZDZkYjU5fDFiYzQ0NTk1OWFiYTRmYzNiOGVjN2I5NGE1NTg2ZmRjfDF8MHw2Mzg5NzExMTAxODk1ODk0MDd8VW5rbm93bnxUV0ZwYkdac2IzZDhleUpGYlhCMGVVMWhjR2tpT25SeWRXVXNJbFlpT2lJd0xqQXVNREF3TUNJc0lsQWlPaUpYYVc0ek1pSXNJa0ZPSWpvaVRXRnBiQ0lzSWxkVUlqb3lmUT09fDB8fHw%3d&sdata=dzRiOTQ1R3pYQVNjR1BwWkNxeTJGbHFwakM5UHE2T2JlUmJIcWxHeWdiZz0%3d",
-      "src/assets/LFA POSTER FHAA_FH-v10.jpg"
+      Poster
     ),
     new PresentationObject(
       "CFHR5 Nephropathy",
